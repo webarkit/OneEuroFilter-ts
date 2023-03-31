e2b93b89d004b1193648f91c9ca1bb292577af67
Walter Perdan <github@kalwaltart.it>
2023-03-31 23:57:07 +0200

    ESM bundle and  updated package entries

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,69 @@
 'use strict';
 const path = require('path');
 
-module.exports = {
-    devtool: 'inline-source-map',
-	entry: './src/index.ts',
-	output: {
-		filename: 'OneEuroFilter.js',
-		path: path.resolve(__dirname, 'dist'),
-        libraryTarget: "umd",
-	},
-    module: {
+module.exports =(env, argv) => {
+    let devtool = false;
+    if (argv.mode === "development") {
+        devtool = "inline-source-map";
+    }
+    console.log(`${argv.mode} build`);
+    const module = {
         rules: [
             {
                 test: /\.([cm]?ts|tsx)$/,
-                loader: 'ts-loader',
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "ts-loader",
+                    },
+                ],
             },
         ],
-    },
-    resolve: {
-        extensions: [ '.ts', '.tsx', '.js' ],
-        extensionAlias: {
-            '.ts': ['.js', '.ts'],
-            '.cts': ['.cjs', '.cts'],
-            '.mts': ['.mjs', '.mts']
+       
+    };
+    return [
+        {
+            name: 'OneEuroFilter-umd',
+            devtool,
+            entry: './src/index.ts',
+            output: {
+                filename: 'OneEuroFilter.js',
+                path: path.resolve(__dirname, 'dist'),
+                libraryTarget: "umd",
+            },
+            resolve: {
+                extensions: ['.ts', '.tsx', '.js'],
+                extensionAlias: {
+                    '.ts': ['.js', '.ts'],
+                    '.cts': ['.cjs', '.cts'],
+                    '.mts': ['.mjs', '.mts']
+                }
+            },
+            module
+        },
+        {
+            name: 'OneEuroFilter-esm',
+            devtool,
+            entry: './src/index.ts',
+            experiments: {
+                outputModule: true,
+            },
+            output: {
+                filename: 'OneEuroFilter.mjs',
+                path: path.resolve(__dirname, 'dist'),
+                library: {
+                    type: "module",
+                },
+            },
+            resolve: {
+                extensions: ['.ts', '.tsx', '.js'],
+                extensionAlias: {
+                    '.ts': ['.js', '.ts'],
+                    '.cts': ['.cjs', '.cts'],
+                    '.mts': ['.mjs', '.mts']
+                }
+            },
+            module
         }
-    }
+    ]
 };
